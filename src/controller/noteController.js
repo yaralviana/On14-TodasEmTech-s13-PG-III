8721b341325e9c1ff9616b44620b4f1af7f3a69b
Paula Allemand <paulaalemand@gmail.com>
2021-11-10 14:41:07 -0300

    saturday class code :sparkles:

--- src/controller/noteController.js
+++ src/controller/noteController.js
@@ -1,10 +1,10 @@
 const NoteSchema = require('../models/noteSchema')
+const mongoose = require('mongoose')
 
 const getAll = async (req, res) => {
     try {
         const notes = await NoteSchema.find()
         res.status(200).json(notes)
-
     } catch (error) {
         res.status(500).json({
             message: error.message,
@@ -12,6 +12,53 @@ const getAll = async (req, res) => {
     }
 }
 
+// criar método para cadastrar uma nota 
+const createNote = async (req, res) => {
+    try {
+        const newNote = new NoteSchema({
+            author: req.body.author,
+            title: req.body.title,
+            _id: new mongoose.Types.ObjectId()
+        })
+
+        const savedNote = await newNote.save()
+        res.status(200).json({
+            message: "Nota adicionada com sucesso! (:",
+            savedNote
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+// criar método para atualizar informações de uma nota
+const updateNoteById = async (req, res) => {
+    try {
+        const findNote = await NoteSchema.findById(req.params.id)
+        console.log(findNote)
+
+        if (findNote) {            
+            findNote.author = req.body.author || findNote.author
+            findNote.title = req.body.title || findNote.title
+        }
+
+        const savedNote = await findNote.save()
+        console.log('APÓS ATUALIZAÇÃO', savedNote)
+
+        res.status(200).json({
+            message: "Nota atualizada com sucesso!!!!",
+            savedNote
+        })
+
+    } catch (error) {
+        
+    }
+}
+
 module.exports = {
-    getAll
+    getAll,
+    createNote,
+    updateNoteById
 }
--- src/routes/noteRoutes.js
+++ src/routes/noteRoutes.js
@@ -3,6 +3,13 @@ const router = express.Router()
 
 const controller = require("../controller/noteController")
 
+// DEMANDA: visualizar todas as notas cadastradas
 router.get("/all", controller.getAll)
+
+// DEMANDA: cadastrar nota
+router.post("/create", controller.createNote)
+
+// DEMANDA: atualizar uma nota
+router.patch("/update/:id", controller.updateNoteById)
 
 module.exports = router
